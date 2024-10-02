7285d88d4ab54ab2cbb554b2f3801687243bd2fb
Phuriwat Siraorarnroj <phuriwats@Phuriwats-MacBook-Pro.local>
2024-10-02 20:50:20 +0700

    feat: init nestjs project and docker-compose

---
+++ backend/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env.local',
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
